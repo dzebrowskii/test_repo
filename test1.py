2ab5ce4d34687deccb3441ff52dc85077b35954b
Maciej1302 <134784694+Maciej1302@users.noreply.github.com>
2024-07-17 16:01:25 +0200

    dasasdsdaadsads

--- test1.py
+++ test1.py
@@ -1,18 +1,53 @@
-import numpy as np
-import pandas as pd - to wyjeb many
-
-
 a = 'test1'
-
+a = 'test1'
+a = 'test1'
+a = 'test1'
+a = 'test1'
+a = 'test1'
+a = 'test1'
+print(x to wyjeb)
+print(x to wyjeb)
+print(x to wyjeb)
+print(x to wyjeb)
+b = 'test2'
+b = 'test2'
+b = 'test2'
+b = 'test2'
+b = 'test2'
+b = 'test2'
 b = 'test2'
 
 c = 'test3 zmień to'
-
+b = 'test2'
+b = 'test2'
 d =' wyjeb to'
+d =' wyjeb to'
+d =' wyjeb to'
+d =' wyjeb to'
+d =' wyjeb to'
+d =' wyjeb to'
+d =' wyjeb to'
+import numpy as np
+import pandas as pd - to wyjeb many
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+
 
 
 e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
+e = 'pousuwaj tutaj spacje'
 
 f =' to zostaw'
+f =' to zostaw'
+f =' to zostaw'
+f =' to zostaw'
+
+
 
 print(x to wyjeb)
