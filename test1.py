e77acdddfc7dbb781fdff647925e9cd6fb77284d
Dominik <frexorplay@gmail.com>
2024-07-17 15:44:41 +0200

    commit testowy od manego

--- test1.py
+++ test1.py
@@ -1,10 +1,24 @@
-
 import numpy as np
 
 print(np.sqrt(16))
 
+#zamieniona linijka kodu
 print('many zmień tą linijke v')
 
+
+
+
+#to trzeba zostawic
+
+#to trzeba usunac
+
+
+
+#to trzeba zostawic
+
+
 x = 'many zmien tą linijke też'
+#to trzeba usunac
+#to trzeba zostawic
 
 print(x)
